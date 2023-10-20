1f1fabc19e1766b7eac9348933b2412568304afd
shabil15 <mohammedshabil15@gmail.com>
2023-10-21 00:52:24 +0530

    cart Schema created

--- models/userModel.js
+++ models/userModel.js
@@ -42,13 +42,39 @@ token:{
 }
 );
 
-
+const cartSchema = new mongoose.Schema({
+  user: {
+    type:mongoose.Schema.Types.ObjectId,
+    ref:'User',
+    required:true
+  },
+  products:[
+    {
+      product: {
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'product',
+        required: true
+      },
+      quantity : {
+        type: Number,
+        default:1,
+        min:1,
+        max:20
+      }
+    }
+  ]
+},
+{
+  timestamps:true
+})
 
 
 
 const User = mongoose.model('User', UserSchema);
+const Cart = mongoose.model('Cart',cartSchema)
 
 module.exports = {
-  User
+  User,
+  Cart
 }
 
